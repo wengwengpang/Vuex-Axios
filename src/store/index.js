bcbe94f7bc16c520c76443f4ec2d45f32e4d6279
Michael_Pang <wengwengpang@gmail.com>
2021-07-21 11:40:49 +0800

    apiGetSet2 v1.0.0.1

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from "vuex";
-import * as state from "./state";
+import state from "./state";
 import * as getters from "./getters";
 import * as mutations from "./mutations";
 import * as actions from "./actions";
